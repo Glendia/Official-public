d9901e5ca61f7f2a3de70dd47e7bb16b74488b09
Glendia <Glendia@users.noreply.github.com>
2024-12-17 17:46:47 +0000

    deploy: 6699f60ab0f1f951f0b4c82a7f3b9a16850a939d

---
+++ assets/error.md.CoLr6rVz.lean.js
@@ -0,0 +1,10 @@
+import{_ as t,c as n,j as r,o}from"./chunks/framework.COVrGdev.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"错误页面"},"headers":[],"relativePath":"error.md","filePath":"error.md"}'),a={name:"error.md"};function s(i,e,l,c,d,p){return o(),n("div",null,e[0]||(e[0]=[r("pre",null,[r("code",null,`<div class='newContent'>
+    <p><br/></p>
+    <p><br/></p>
+    <p><br/></p>
+    <p style='text-align:center'>
+        <img src='https://g.alicdn.com/quark-cloud-drive/quark-cloud-drive-share-h5/0.0.79/assets/18bc853b523de647a0fd.png' style='max-width:10rem'>
+    </p>
+    <div style='text-align:center; font-size:1rem; line-height:1.8rem; color:#333333;'>很抱歉，您访问的页面不存在！</div>
+    <div style='text-align:center; font-size=1rem; line-height:1.8rem; color:#333333;'>请仔细检查您输入的网址是否正确。</div>
+`)],-1)]))}const f=t(a,[["render",s]]);export{u as __pageData,f as default};
