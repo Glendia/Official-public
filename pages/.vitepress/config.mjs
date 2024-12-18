b4c0a8c45c914559fe0e9e4589e6e8d7beab78c3
Evan <evanyanet@gmail.com>
2024-12-18 22:32:44 +0800

    agreement

--- pages/.vitepress/config.mjs
+++ pages/.vitepress/config.mjs
@@ -8,8 +8,8 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '首页', link: '/' },
+      { text: '法律协议', link: '/agreement' }
     ],
 
     sidebar: [
