0efcc741a148aa63e2762ace70984b25f2436299
Glendia <Glendia@users.noreply.github.com>
2024-12-19 05:55:09 +0000

    deploy: 790764bb9c84a23c4dde72a785a0833f503fab1b

---
+++ assets/team.md.D9EhwjJM.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.COVrGdev.js";const i=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"team.md","filePath":"team.md"}'),r={name:"team.md"};function o(s,c,n,m,p,_){return a(),t("div")}const f=e(r,[["render",o]]);export{i as __pageData,f as default};
